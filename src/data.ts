04fba8f91ba403276f86f6b2fe505d8fe37de8b5
Carlos A. <carlosvsk@gmail.com>
2024-09-12 16:28:06 -0300

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,8 @@
+export const proxy =
+  'https://us-central1-flaxboll.cloudfunctions.net/proxyCall?url=';
+export const baseUrl = 'https://api.pagar.me/core/v5';
+export const secrets = {
+  customerId: '',
+  sk: '',
+  pk: '',
+};
