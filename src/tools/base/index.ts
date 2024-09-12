779677811713a4208087a67581832c83e6369719
Carlos A. <carlosvsk@gmail.com>
2024-09-12 17:04:40 -0300

    Commit generated by Flaxboll.

--- src/tools/base/index.ts
+++ src/tools/base/index.ts
@@ -3,18 +3,17 @@
 import * as functions from './functions';
 import * as Elements from './Elements';
 import * as project from './project';
-// import * as jsvals from './jsvals';
-// import * as props from './props';
-// import * as stls from './stls';
+import * as jsvals from './jsvals';
+import * as props from './props';
+import * as stls from './stls';
 
 export const base = {
   ...project,
   functions,
   Elements,
 
-  // jsvals,
+  jsvals,
 
-  // stls,
-  // props,
+  stls,
+  props,
 };
-
