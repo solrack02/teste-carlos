b160b0497476073632d85ebb6ff1417fa5fd6b9f
Carlos A. <carlosvsk@gmail.com>
2024-10-01 16:19:26 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,40 @@
           let args: any = [];
 
           const screens = [
-            // []
+            // 
+
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"home",
+
+          styles:[
+              {
+                backgroundColor: '#101',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
+              }
+              ],
+
+          screenElements:[() => {
+                  const textStyle = {
+                    fontSize: 20,
+                    color: '#fff2',
+                    textAlign:'center',
+                    maxWidth: 200,
+                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
+                  };
+
+                  return (
+                    <RN.Text style={textStyle}>
+                      {'Adicione Elementos nessa tela!'}
+                    </RN.Text>);
+                }],
+
+          startFunctions:[()=>{}],
+
+          args,
+        }}/>
           ];
 
           const initCt = () => ({
