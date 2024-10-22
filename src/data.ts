cdd95fda9ac880bc65097c3e6c44eaae1edd73d2
Carlos A. <carlosvsk@gmail.com>
2024-10-22 16:28:57 -0300

    Delete src/data.ts

--- src/data.ts
+++
@@ -1,8 +0,0 @@
-export const proxy =
-  'https://us-central1-flaxboll.cloudfunctions.net/proxyCall?url=';
-export const baseUrl = 'https://api.pagar.me/core/v5';
-export const secrets = {
-  customerId: '',
-  sk: '',
-  pk: '',
-};
